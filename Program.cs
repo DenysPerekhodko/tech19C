6baed49637eecacfe0ad9d7884955c1d5360c52b
22DP <114557893+szkolajestsup@users.noreply.github.com>
2022-10-18 22:39:11 +0200

    karta_pracy2a

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿//zad1
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//if ((a + b) % 2 == 0)
+//{
+//Console.WriteLine("liczba jest parzysta");
+//}
+//else
+//{
+//Console.WriteLine("Liczba nie jest parzysta");
+//}
+//zad2
+//int c = int.Parse(Console.ReadLine());
+//int d = int.Parse(Console.ReadLine());
+//if ((c + d) / 2 < Math.Sqrt(a * b))
+//{
+//    Console.WriteLine("Tak");
+//}
+//else
+//{
+//    Console.WriteLine("Nie");
+//}
+//zad3
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());  
+//int c = int.Parse(Console.ReadLine());
+//Console.WriteLine((a + b + c)/3);
+//if (a == b || b == c || a == c)
+//{
+//    if (a == c)
+//    {
+//        Console.WriteLine("Tak a = c");
+//    }
+//    if (b == c)
+//    {
+//        Console.WriteLine("Tak a = b");
+//    }
+//    if (a == b)
+//    {
+//        Console.WriteLine("a = b");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Nie")
+//}
+//zad4
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());  
+//int d = int.Parse(Console.ReadLine());
+//if (a<b && a>c && a<d)
+//{
+//    Console.WriteLine("a jest najmniejsze");
+//}
+//else if (b<c && b<d && b<a)
+//{
+//    Console.WriteLine("b jest najmniejsze");
+//}
+//else if (c<d && c<a && c<b)
+//{
+//    Console.WriteLine("c jest najmniejsze");
+//}
+//else if (d<c && c<a && c<b)
+//{
+//    Console.WriteLine("d jest najmniejsze");
+//}
+//zad5
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (a - b < c < a + b && b - c < a <b + c && a - c < b < a + c)
+//{
+//    Console.WriteLine("Tak");
+//}
+//else
+//{
+//    Console.WriteLine("Nie");
+//}
+//zad6
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//if (a + b > c && b + c > a && c + a > b)
+//{
+//    if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2) || Math.Pow(b, 2) + Math.Pow(c, 2) == Math.Pow(a, 2) || Math.Pow(a, 2) + Math.Pow(c, 2) == Math.Pow(b, 2))
+//    {
+//        Console.WriteLine("prosto-");
+//    }    
+//    if (Math.Pow(a, 2) + Math.Pow(b, 2) < Math.Pow(c, 2) || Math.Pow(b, 2) + Math.Pow(c, 2) < Math.Pow(a, 2) || Math.Pow(a, 2) + Math.Pow(c, 2) < Math.Pow(b, 2))
+//    {
+//        Console.WriteLine("rozwarto-");
+//    }
+//    if (Math.Pow(a, 2) + Math.Pow(b, 2) > Math.Pow(c, 2) || Math.Pow(b, 2) + Math.Pow(c, 2) > Math.Pow(a, 2) || Math.Pow(a, 2) + Math.Pow(c, 2) > Math.Pow(b, 2))
+//    {
+//        Console.WriteLine("ostro-");
+//    }
+
+
+
+//}
